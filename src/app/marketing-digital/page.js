b57fc7b0b13825ed6430dbfdef8a0a164666750e
Tristan Wehrle <tastroplaragebaaah@mbp-de-tristan.home>
2025-10-21 13:42:29 +0200

    [FIX] trying to solve again deployement :/

--- src/app/marketing-digital/page.js
+++ src/app/marketing-digital/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import Header from "@/components/Header";
+import Header from "@/components/Heade.js";
 
 
 export default function MarketingDigital() {
