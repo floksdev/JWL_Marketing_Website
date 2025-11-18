dfffa857ffa0e63b261ed1aa5a7d222f77a0c5e9
Tristan Wehrle <tastroplaragebaaah@mbp-de-tristan.home>
2025-11-18 10:28:09 +0100

    [ADD] GTM Jodie

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,7 +19,7 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "JWL Marketing",
   description: "JWL Marketing - Votre partenaire en marketing digital",
-    icons: {
+  icons: {
     icon: "/favicon.ico",
     shortcut: "/favicon.ico",
     apple: "/apple-touch-icon.png",
@@ -34,18 +34,44 @@ export default function RootLayout({ children }) {
           rel="stylesheet"
           href="https://assets.calendly.com/assets/external/widget.css"
         />
+
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id=GTM-PZ96DBWV'+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-PZ96DBWV');
+            `,
+          }}
+        />
+
         <Script
           src="https://assets.calendly.com/assets/external/widget.js"
           strategy="afterInteractive"
         />
       </head>
-      <body className={`bg-[#F8F8F8] ${geistSans.variable} ${geistMono.variable} antialiased`}>
+
+      <body
+        className={`bg-[#F8F8F8] ${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {/* --- GTM NOSCRIPT (juste après ouverture du body) --- */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PZ96DBWV"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         <CartProvider>
           <div className="flex min-h-screen flex-col">
             <Header />
-            <div className="flex-1">
-              {children}
-            </div>
+            <div className="flex-1">{children}</div>
             <Foorter />
           </div>
           <CartDrawer />
